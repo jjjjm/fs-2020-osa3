5b9b6a1714590c8cee81aad932c8acad24363fb9
Jere Mustonen <jere.mustonen@helsinki.fi>
2020-02-10 07:08:56 +0200

    heroku

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 
+const Person = require('./models/person')
 
 const morgan = require('morgan')
-morgan.token('body',  (req, res) =>  {
-    if(req.method === 'POST'){
+morgan.token('body', (req, res) => {
+    if (req.method === 'POST') {
         return JSON.stringify(req.body)
     }
     return ''
@@ -16,73 +18,74 @@ app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.json())
 
-
-
-
-let persons = [
-    {
-        name: 'Arto Hellas',
-        number: '040-123456',
-        id: 1
-    },
-    {
-        name: 'Ada Lovelace',
-        number: '39-44-5323523',
-        id: 2
-    },
-    {
-        name: 'Dan Abramov',
-        number: '12-43-234345',
-        id: 3
-    },
-    {
-        name: 'Mary Poppendick',
-        number: '39-23-6423122',
-        id: 4
-    }
-]
-
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
+app.get('/api/persons', (req, res, next) => {
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
-
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id)
+        .then(person => res.json(person).toJSON())
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req,res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
-    if (!body.name || !body.number) {
-        res.status(400).json({ error: 'fields name, number must be included' })
-    } else if (persons.find(person => person.name === body.name)) {
-        res.status(400).json({ error: 'name must be unique' })
-    } else {
-        const newPerson = {...body, id: Math.floor(Math.random() * 1000000)}
-        persons = persons.concat(newPerson)
-        res.json(newPerson)
-    }
+    const newPerson = new Person({ ...body })
+    newPerson.save()
+        .then(savedPerson => res.json(savedPerson).toJSON())
+        .catch(error => next(error))
 })
 
-app.get('/info', (req, res) => {
-    res.send(`<div>Phonebook has info for ${persons.length} people</div> 
-              </br>
-              <div>${new Date()}</div>`)
+app.put('/api/persons/:id', (req, res, next) => {
+    const person = { ...req.body }
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => res.json(updatedPerson))
+        .catch(error => next(error))
 })
-const PORT = process.env.PORT || 3001
+
+app.get('/info', (req, res, next) => {
+    Person.find({}).then(persons => {
+        res.send(`<div>Phonebook has info for ${persons.length} people</div> </br><div>${new Date()}</div>`)
+    })
+        .catch(error => next(error))
+})
+
+
+
+//ERROR MIDDLEWARE
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError' && error.kind == 'ObjectId') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+    next(error)
+}
+
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+
+// virheellisten pyyntöjen käsittely
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT || process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fs2020:${password}@cluster0-olys7.mongodb.net/test?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length < 5) {
+    Person.find({}).then(persons => {
+        console.log('phonebook:')
+        persons.forEach(person => console.log(person.name, person.number))
+        mongoose.connection.close()
+    })
+} else {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+    person.save().then(result => {
+        console.log('added', person.name, 'number', person.name, 'to phonebook')
+        mongoose.connection.close()
+    })
+}
+
+
+
+
